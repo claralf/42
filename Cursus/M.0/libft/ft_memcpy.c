5c0f56acbea7950278cec366c5e9e6958f700243
claralf <claralf@gmail.com>
2025-10-09 11:27:17 +0200

    ay

--- Cursus/M.0/libft/ft_memcpy.c
+++ Cursus/M.0/libft/ft_memcpy.c
@@ -1,41 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: clalfons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/03 15:10:33 by clalfons          #+#    #+#             */
-/*   Updated: 2025/10/03 15:33:55 by clalfons         ###   ########.fr       */
+/*   Created: 2025/10/07 23:56:45 by clalfons          #+#    #+#             */
+/*   Updated: 2025/10/07 23:56:48 by clalfons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-#include <stdio.h>
-#include <string.h>
-
-int	main(void)
-{
-	char	*s = "hola";
-	int		libft;
-	int		original;
-
-	libft = ft_strlen(s);
-	original = strlen(s);
-	printf("resultado: %d\n", libft);
-	printf("resultado original: %d\n", original);
-	return (0);
-}
