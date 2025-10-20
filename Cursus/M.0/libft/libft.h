a954a5d8bf665737f3e58567fcf411b8cb866bae
claralf <claralf@gmail.com>
2025-10-20 18:05:11 +0200

    update

--- Cursus/M.0/libft/libft.h
+++ Cursus/M.0/libft/libft.h
@@ -17,8 +17,8 @@
 
 #include <stdio.h> //Aparentemente estas las borramos antes de entregar
 #include <ctype.h>
-#include <bsd/string.h>
-//#include <strings.h>
+#include <string.h>
+#include <strings.h> //Maybe not>
 #include <unistd.h>
 #include <stdlib.h> //Malloc
 #include <stddef.h> //size_t
@@ -35,5 +35,9 @@ void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
-int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 #endif
