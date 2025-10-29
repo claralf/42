13bb7ff220d81a6632da01795ba1e12ebc1c25f9
claralf <claralf@gmail.com>
2025-10-29 01:39:20 +0100

    la mapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,40 @@
+//#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int	i;
+	int	len;
+	char	*sol;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	sol = malloc(sizeof(char) * len + 1);
+	if (!sol)
+		return (NULL);
+	while (s[i])
+	{
+		sol[i] = f(i, s[i]);
+		i++;
+	}
+	return (sol);
+}
+
+static char	ft_ft(unsigned int i, char c)
+{
+	if (i % 2 == 0)
+		return (ft_tolower(c);
+	return (ft_toupper(c);
+
+}
+
+int	main(void)
+{
+	char 	*s = "hola";
+	
+	printf("%s\n", s);
+	printf("%s\n", ft_strmapi(s, ft_ft);
+	return (0);
+}
