1f9949a336141d95a188490c33b300b1ec9a6d9d
claralf <claralf@gmail.com>
2025-11-29 05:48:14 +0100

    holi

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -4,25 +4,29 @@
 int	ftype(char type, va_list *args)
 {
 	if (type == '%')
-		return(ft_putchar_fd("%", 1));
+		return (ft_putchar_fd("%", 1));
 	else if (type == 'c')
-		return(ft_putchar_fd(type, 1));
+		return (ft_putchar_fd(type, 1));
 	else if (type == 's')
-		return(ft_putstr_fd(va_arg(args, char *), 1));
+		return (ft_putstr_fd(va_arg(args, char *), 1));
 	else if (type == 'd' || type == 'i')
-		return(ft_putnbr_fd(va_arg(args, int), 1));
+		return (ft_putnbr_fd(va_arg(args, int), 1));
 	else if (type == 'x')
-		return(ft_putnbr_base_fd(va_arg(args, unsigned long), 1));
+		return (ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789abcdef", 1));
 	else if (type == 'X')
-		return(ft_putnbr_base_fd(va_arg(args, unsigned long), 1));
+		return (ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789ABCDEF", 1));
 	else if (type == 'u')
-		return(ft_putnbr_base_fd(va_arg(args, unsigned long), 1));
+		return (ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789", 1));
 	else if (type == 'p')
-		return(ft_putnbr_base_fd(va_arg(args, unsigned long), 1));
+	{
+		write(1, "0x", 2);
+		return ((ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789abcdef", 1) + 2));
+	}
 }
-int ft_printf(const char *s, ...)
+
+int	ft_printf(const char *s, ...)
 {
-	va_list	args;
+	va_list			args;
 	int	cnt;
 	int	i;
 
@@ -31,14 +35,14 @@ int ft_printf(const char *s, ...)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == '%' && s[i+1] != 0)
+		if (s[i] == '%' && s[i + 1] != 0)
 		{
 			i++;
-			cnt = cnt + ftype(s[i], args);	
+			cnt = cnt + ftype(s[i], args);
 		}
 		else
 		{
-			ft_putchar_fd(s[i], fd);
+			ft_putchar_fd(s[i], 1);
 			cnt++;
 		}
 		i++;
@@ -47,10 +51,11 @@ int ft_printf(const char *s, ...)
 	return (cnt);
 }
 
-int main(void)
+int	main(void)
 {
 	int	hola = 19999999;
 	char	*hola2 = "hahahahahahhahaha :)";
+
 	printf("%d\n%i\n%s\n hola clara", hola, hola, hola2);
-	return 0;
+	return (0);
 }
--- ft_printf/funciones1.c
+++ ft_printf/funciones1.c
@@ -10,11 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
+#include <stdio.h>
 
 int	ft_strlen(const char *s)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
@@ -23,20 +23,28 @@ int	ft_strlen(const char *s)
 	}
 	return (i);
 }
+
 int	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 	return (1);
 }
+
 int	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
 
 	i = -1;
+	if (!s)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	while (s[++i])
 		ft_putchar_fd(s[i], fd);
 	return (ft_strlen(s));
 }
+
 int	ft_putnbr_fd(int n, int fd)
 {
 	int	cnt;
@@ -54,16 +62,17 @@ int	ft_putnbr_fd(int n, int fd)
 	}
 	if (n > 9)
 		cnt = cnt + ft_putnbr_fd(n / 10, fd);
-	cnt = cnt + ft_putchar_fd((n % 10) + '0', fd);
+	cnt = cnt + ft_putchar_fd((n % 10) + 48, fd);
 	return (cnt);
 }
-int	ft_putnbr_base_fd(unsigned long n, int fd)
+
+int	ft_putnbr_base_fd(unsigned long n, char *base, int fd)
 {
 	int	cnt;
 
 	cnt = 0;
 	if (n > 9)
-		cnt = cnt + ft_putnbr_fd(n / 10, fd);
-	cnt = cnt + ft_putchar_fd((n % 10) + '0', fd);
+		cnt = cnt + ft_putnbr_fd((n / ft_strlen(base)), fd);
+	cnt = cnt + ft_putchar_fd((n % ft_strlen(base)) + 48, fd);
 	return (cnt);
 }
