b50717b02b075e8eb0850371298b1cf6d69d5fa6
claralf <claralf@gmail.com>
2025-11-30 01:11:02 +0100

    holi

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
-#include <stdarg.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clalfons <clalfons@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 21:49:12 by clalfons          #+#    #+#             */
+/*   Updated: 2025/11/11 04:58:08 by clalfons          ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ftype(char type, va_list *args)
+#include "ft_printf.h"
+
+int	ftype(char type, va_list args)
 {
 	if (type == '%')
-		return (ft_putchar_fd("%", 1));
+		return (ft_putchar_fd('%', 1));
 	else if (type == 'c')
 		return (ft_putchar_fd(type, 1));
 	else if (type == 's')
@@ -12,23 +23,28 @@ int	ftype(char type, va_list *args)
 	else if (type == 'd' || type == 'i')
 		return (ft_putnbr_fd(va_arg(args, int), 1));
 	else if (type == 'x')
-		return (ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789abcdef", 1));
+		return (ft_putnbr_base_fd(va_arg(args, unsigned long),
+				/ "0123456789abcdef", 1));
 	else if (type == 'X')
-		return (ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789ABCDEF", 1));
+		return (ft_putnbr_base_fd(va_arg(args, unsigned long),
+				/ "0123456789ABCDEF", 1));
 	else if (type == 'u')
-		return (ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789", 1));
+		return (ft_putnbr_base_fd(va_arg(args, unsigned long),
+				/ "0123456789", 1));
 	else if (type == 'p')
 	{
 		write(1, "0x", 2);
-		return ((ft_putnbr_base_fd(va_arg(args, unsigned long), "0123456789abcdef", 1) + 2));
+		return ((ft_putnbr_base_fd(va_arg(args, unsigned long),
+					/ "0123456789abcdef", 1) + 2));
 	}
+	return (-1);
 }
 
 int	ft_printf(const char *s, ...)
 {
 	va_list			args;
-	int	cnt;
-	int	i;
+	int				cnt;
+	int				i;
 
 	va_start(args, s);
 	cnt = 0;
@@ -47,15 +63,17 @@ int	ft_printf(const char *s, ...)
 		}
 		i++;
 	}
-	va_ends(args);
+	va_end(args);
 	return (cnt);
 }
 
-int	main(void)
+/*int	main(void)
 {
-	int	hola = 19999999;
+	int	hola = -19999999;
 	char	*hola2 = "hahahahahahhahaha :)";
+	int	*hola3 = &hola;
+	int	hola4 = -4;
 
-	printf("%d\n%i\n%s\n hola clara", hola, hola, hola2);
+	printf("%d\n%i\n%s\n %% %p %u hola clara", hola, hola, hola2, hola3, hola4);
 	return (0);
-}
+}*/
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clalfons <clalfons@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 21:49:12 by clalfons          #+#    #+#             */
+/*   Updated: 2025/11/11 04:58:08 by clalfons          ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+int	ft_printf(const char *s, ...);
+int	ftype(char type, va_list args);
+int	ft_strlen(const char *s);
+int	ft_putchar_fd(char c, int fd);
+int	ft_putstr_fd(char *s, int fd);
+int	ft_putnbr_fd(int n, int fd);
+int	ft_putnbr_base_fd(unsigned long n, char *base, int fd);
+
+#endif
