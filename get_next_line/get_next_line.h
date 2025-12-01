1c4b93aeb47b7daf939fed288e5da6a5dc060c26
claralf <claralf@gmail.com>
2025-12-01 21:46:33 +0100

    ghjrkahl

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -18,6 +18,9 @@
 # include <stdio.h>
 # include <limits.h>
 
-int	ft_putnbr_base_fd(unsigned long n, char *base, int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
