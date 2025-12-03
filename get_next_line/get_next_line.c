2cb80aa7fcfca5a64900022f73554cc525c330ab
claralf <claralf@gmail.com>
2025-12-03 04:07:59 +0100

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,9 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*read_more(char *temp, int fd)
+{
+	char	*buffer;
+	int	read;
+
+	read = 0;
+	buffer = malloc(sizeof(char)*(BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	// el 0 es nulo
+	while (read && !ft_strchr(buffer, '\n')
+	{
+		read = read(fd, buffer, BUFFER_SIZE);
+		if (read)
+		{
+			buffer[read] = '\0';
+			temp = ft_strjoin(temp, buffer);
+		}
+	}
+	free (buffer);
+	// si he leído negativo, libero temp. Sino, devuelvo temp
+}
+
 char *get_next_line(int fd)
 {
-	
+	static char	*temp;
+
+	*temp = NULL;
+	if (!fd)
+		return (NULL);
+	if (!temp || !ft_strchr(temp, '\n');
+		temp = read_more(temp, fd);
+	if (!temp)
+		return (NULL);	
+	// cogemos una linea del almac. con FUNCION2(almacenamiento)
+	// si se ha hecho mal, liberamos almacenamiento con FUNCION3
+	//modificamos el almacenamiento con FUNCION4, quitandole lo que hay aentes del primer salto de linea.	
 }
 
 /*
